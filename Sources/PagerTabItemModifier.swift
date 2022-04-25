cde958885606df8c87e8b84742d597eb4090fa57
huseyinbagana <30414455+huseyinbagana@users.noreply.github.com>
2022-04-25 12:21:30 +0300

    bug fixed.

--- Sources/PagerTabItemModifier.swift
+++ Sources/PagerTabItemModifier.swift
@@ -21,7 +21,13 @@ struct PagerTabItemModifier<NavTabView: View>: ViewModifier {
                 .onAppear {
                     DispatchQueue.main.async {
                         let frame = reader.frame(in: .named("PagerViewScrollView"))
-                        index = Int(round(frame.minX / frame.width))
+                        if round(frame.minX / frame.width).isNaN || round(frame.minX / frame.width).isInfinite {
+                            
+                        }
+                        else {
+                            index = Int(round(frame.minX / frame.width))
+                        }
+                        
                         let tabView = navTabView()
                         let tabViewDelegate = tabView as? PagerTabViewDelegate
                         dataStore.setView(AnyView(tabView), at: index)
